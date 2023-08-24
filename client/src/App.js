88c8be3a932f8e0c62b10ce383ca02b5a3f6bee2
David Mondragon <dragon18saiz@gmail.com>
2023-08-24 12:02:00 -0600

    Initial commit - Basic MERN structure -

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ server/utils/auth.js
@@ -0,0 +1,38 @@
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretssshhhhhhh';
+const expiration = '2h';
+
+module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch (err) {
+      console.error(err);
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ email, firstname, lastName, area, _id }) {
+    const payload = { email, firstname, lastName, area, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
