ea0df83e7a8a984c6fb2c547908bc9324521f434
David Mondragon <dragon18saiz@gmail.com>
2023-08-28 14:42:21 -0600

    Added login and sign up modals, logic to change between those modals

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -2,16 +2,17 @@ import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import CustomModal from '../components/CustomModal';
 import inboxLogo from '../img/dragonballLogo.svg';
-import gokuLogo from '../img/gokuLogo.svg';
-import { Link } from 'react-router-dom';
+// import gokuLogo from '../img/gokuLogo.svg';
+// import { Link } from 'react-router-dom';
 
 export default function Homepage() {
+  const [showLoginModal, setShowLoginModal] = useState(false);
   const [showSignUpModal, setShowSignUpModal] = useState(false);
   return (
     <div>
       <main className="bg-neutral-950 px-10 md:px-20 lg:px-40">
         <section className="min-h-screen">
-          <Navbar showLoginModal={() => setShowSignUpModal(true)} />
+          <Navbar showLoginModal={() => setShowLoginModal(true)} />
           <div className="flex flex-col mt-10 lg:flex-row lg:justify-evenly">
             <div className="text-slate-200 font-bold text-6xl w-5/6 lg:text-8xl lg:w-3/5">
               <h1 className="font-poppins">
@@ -28,7 +29,7 @@ export default function Homepage() {
               </p>
               <div className="mt-10 flex">
                 <button
-                  // onClick={() => setShowSignUpModal(true)}
+                  onClick={() => setShowSignUpModal(true)}
                   className=" bg-slate-200 text-neutral-950 hover:text-orange-500  hover:bg-orange-200 py-2 px-6 font-bold font-poppins rounded-lg lg:text-lg cursor-pointer hover:scale-125 transition duration-300 ease-in-out"
                 >
                   Get Started
@@ -63,8 +64,8 @@ export default function Homepage() {
         </section>
       </main>
       <CustomModal
-        isVisible={showSignUpModal}
-        onClose={() => setShowSignUpModal(false)}
+        isVisible={showLoginModal}
+        onClose={() => setShowLoginModal(false)}
       >
         <div className="py-6 px-6 lg:px-8 text-left">
           <h3 className="mb-4 text-xl font-medium text-gray-900">
@@ -105,12 +106,95 @@ export default function Homepage() {
             </button>
             <div className="text-sm font-medium text-gray-500">
               Not registered?{' '}
-              <a
-                href="#"
+              <button
+                onClick={() => {
+                  setShowLoginModal(false);
+                  setShowSignUpModal(true);
+                }}
                 className="text-orange-500 hover:underline hover:text-gray-500"
               >
                 Create account
-              </a>
+              </button>
+            </div>
+          </form>
+        </div>
+      </CustomModal>
+      <CustomModal
+        isVisible={showSignUpModal}
+        onClose={() => setShowSignUpModal(false)}
+      >
+        <div className="py-6 px-6 lg:px-8 text-left">
+          <h3 className="mb-4 text-xl font-medium text-gray-900">
+            Create your account!
+          </h3>
+          <form className="space-y-6" action="#">
+            <div>
+              <label for="username" className=""></label>{' '}
+              <input
+                type="username"
+                name="username"
+                id="username"
+                className="bg-gra-50 border border-gray-300 text-gray-900 text-sm 
+                rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5"
+                placeholder="Username"
+                required
+              ></input>
+            </div>
+            <div>
+              <label for="email" className=""></label>{' '}
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="bg-gra-50 border border-gray-300 text-gray-900 text-sm 
+                rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5"
+                placeholder="name@email.com"
+                required
+              ></input>
+            </div>
+            <div>
+              <label for="password" className=""></label>{' '}
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="bg-gra-50 border border-gray-300 text-gray-900 text-sm 
+                rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5"
+                placeholder="Enter password"
+                required
+              ></input>
+            </div>
+            <div>
+              <label for="confirmPassword" className=""></label>{' '}
+              <input
+                type="confirmPassword"
+                name="confirmPassword"
+                id="confirmPassword"
+                className="bg-gra-50 border border-gray-300 text-gray-900 text-sm 
+                rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5"
+                placeholder="Confirm password"
+                required
+              ></input>
+            </div>
+            <button
+              type="submit"
+              className="w-full text-white bg-orange-700 hover:bg-orange-800 
+              focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium
+              rounde-lg text-sm px-5 py-2.5 text-center"
+            >
+              Create account
+            </button>
+            <div className="text-sm font-medium text-gray-500">
+              Already have an account?{' '}
+              <button
+                onClick={() => {
+                  setShowSignUpModal(false);
+                  setShowLoginModal(true);
+                }}
+                className="text-orange-500 hover:underline hover:text-gray-500"
+              >
+                Log in to your account
+              </button>
             </div>
           </form>
         </div>
