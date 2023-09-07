1ba76d64e356146debbe04334a042af545798638
David Mondragon <dragon18saiz@gmail.com>
2023-09-06 18:49:14 -0600

    Changed seeds (Server)

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,12 +1,25 @@
+// const db = require('../config/connection');
+// const { User, Figure } = require('../models');
+// const figureData = require('./figuresData.json');
+
+// db.once('open', async () => {
+//   // await User.deleteMany({});
+//   // await Thought.create(thoughtSeeds);
+//   await Figure.deleteMany({});
+//   await Figure.create(figureData);
+//   console.log('all done!');
+//   process.exit(0);
+// });
 const db = require('../config/connection');
 const { User, Figure } = require('../models');
+
 const figureData = require('./figuresData.json');
 
-db.once('open', async () => {
-  // await User.deleteMany({});
-  // await Thought.create(thoughtSeeds);
+const seeds = async () => {
   await Figure.deleteMany({});
   await Figure.create(figureData);
   console.log('all done!');
   process.exit(0);
-});
+};
+
+module.exports = seeds;
